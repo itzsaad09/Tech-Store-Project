fd13f3fcbce439b5b74ce4c69a22df591e6c810e
itzsaad09 <muhammad.saad9788753@gmail.com>
2025-08-09 13:20:26 +0500

    Update auth

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { registerUser, loginUser, adminLogin, userDisplay, getShippingDetails, addShippingDetails} from "../controllers/userController.js";
 import userAuth from "../middleware/userAuth.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const userRoute = express.Router();
 
 userRoute.post("/register", registerUser);
 userRoute.post("/login", loginUser);
 userRoute.post("/admin", adminLogin);
-userRoute.get("/display", userDisplay);
+userRoute.get("/display", adminAuth ,userDisplay);
 userRoute.get("/shipping/:userId", getShippingDetails);
 userRoute.post("/addshipping", addShippingDetails);
 
